e4b3e8a41d09b01bf5e58bc97782889938202d25
Bruno Cabral <cabral.brc@gmail.com>
2022-04-03 17:16:21 -0700

    single player class

--- main.py
+++ main.py
@@ -13,17 +13,40 @@ def debug(message: str):
 
 
 class Player:
-    def __init__(self, name: str, color: pygame.color.Color):
-        super().__init__()
+    def __init__(
+        self,
+        name: str = "P1",
+        color: pygame.color.Color = "blue",
+        left_key: int = pygame.K_a,
+        right_key: int = pygame.K_d,
+        up_key: int = pygame.K_w,
+        down_key: int = pygame.K_s,
+    ):
+        # setup
         self.name = name
         self.color = color
         self.speed = 60
 
+        # position
         self.pos = pygame.Vector2(random.randint(20, 620), 10)
         self.direction = pygame.Vector2(0, 0)
 
+        # control
+        self.left_key = left_key
+        self.right_key = right_key
+        self.up_key = up_key
+        self.down_key = down_key
+
     def player_input(self, dt: float):
-        pass
+        keys = pygame.key.get_pressed()
+        if keys[self.left_key]:
+            self.move_left(dt)
+        if keys[self.right_key]:
+            self.move_right(dt)
+        if keys[self.up_key]:
+            self.move_up(dt)
+        if keys[self.down_key]:
+            self.move_down(dt)
 
     def move_left(self, dt: float):
         self.direction -= pygame.Vector2(self.speed * dt, 0)
@@ -43,32 +66,6 @@ class Player:
             self.direction = self.direction.normalize()
         self.pos += self.direction
         self.direction = pygame.Vector2(0, 0)
-
-
-class Player1(Player):
-    def player_input(self, dt: float):
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_a]:
-            self.move_left(dt)
-        if keys[pygame.K_d]:
-            self.move_right(dt)
-        if keys[pygame.K_w]:
-            self.move_up(dt)
-        if keys[pygame.K_s]:
-            self.move_down(dt)
-
-
-class Player2(Player):
-    def player_input(self, dt: float):
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_j]:
-            self.move_left(dt)
-        if keys[pygame.K_l]:
-            self.move_right(dt)
-        if keys[pygame.K_i]:
-            self.move_up(dt)
-        if keys[pygame.K_k]:
-            self.move_down(dt)
 
 
 class GameScene:
@@ -101,6 +98,7 @@ class Scene1(GameScene):
                 self.scene_surface, player.color, (player.pos[0], player.pos[1], 30, 30)
             )
         GameWindow().window_surface.blit(self.scene_surface, (0, 0))
+        GameWindow().window_surface.blit(self.scene_surface, (0, 240))
         debug(player.pos)
 
 
@@ -116,8 +114,10 @@ class GameWindow:
         pygame.init()
         self.window_surface = pygame.display.set_mode((640, 480))
         self.clock = pygame.time.Clock()
-        self.player1 = Player1("P1", "blue")
-        self.player2 = Player2("P2", "orange")
+        self.player1 = Player()
+        self.player2 = Player(
+            "P2", "orange", pygame.K_j, pygame.K_l, pygame.K_i, pygame.K_k
+        )
         self.set_scene()
         GameWindow.instance = self
         return self
